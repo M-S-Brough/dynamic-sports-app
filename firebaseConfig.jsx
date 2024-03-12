e9660d71f9c4098e687affdd2b184ff199cae99e
MarkBrough21 <117440810+MarkBrough21@users.noreply.github.com>
2024-03-12 11:17:24 +0000

    Firebaset setup using Add Post screen

---
+++ firebaseConfig.jsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKCRQOLzolqIYJZdCAjPZ8wHWCl4TaPvA",
+  authDomain: "north-walkden-girls.firebaseapp.com",
+  projectId: "north-walkden-girls",
+  storageBucket: "north-walkden-girls.appspot.com",
+  messagingSenderId: "437518893719",
+  appId: "1:437518893719:web:03a365664827616d5d9fd2",
+  measurementId: "G-MHD4B5KCNN"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
