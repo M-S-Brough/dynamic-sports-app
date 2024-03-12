dafa996516bc3f26fad0aebb5defc2103d1acbf7
MarkBrough21 <117440810+MarkBrough21@users.noreply.github.com>
2024-03-12 10:07:15 +0000

    Authentication

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./Apps/Screens/LoginScreen";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 
 export default function App() {
   return (
-    <View className="flex-1  bg-white">
-      <StatusBar style="auto" />
-      <LoginScreen />
-    </View>
+    <ClerkProvider publishableKey="pk_test_YXJyaXZpbmctc2N1bHBpbi05MS5jbGVyay5hY2NvdW50cy5kZXYk">
+      <View className="flex-1  bg-white">
+        <StatusBar style="auto" />
+
+        <SignedIn>
+          <Text className="mt-10">You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+      </View>
+    </ClerkProvider>
   );
 }
 
