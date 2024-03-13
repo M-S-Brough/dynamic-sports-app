cc8bec4efa39c9b886f817d990e78d3adaaf579c
MarkBrough21 <117440810+MarkBrough21@users.noreply.github.com>
2024-03-13 09:43:38 +0000

    Slider added to home screen

--- Apps/Screens/HomeScreen.jsx
+++ Apps/Screens/HomeScreen.jsx
@@ -1,11 +1,35 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../Components/HomeScreen/Header'
+import Slider from '../Components/HomeScreen/Slider'
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+import { app } from '../../firebaseConfig'
 
 export default function HomeScreen() {
+
+  const db = getFirestore(app);
+  const [sliderList, setSliderList] = useState([]);
+
+  useEffect(() => {
+      getSlider();
+  },[])
+
+  const getSlider = async () => {
+    setSliderList([])
+    const querySnapshot = await getDocs(collection(db, "Slider"));
+    querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  
+  setSliderList(sliderList => [...sliderList, doc.data()]);
+
+});
+
+  }
+
   return (
     <View className="py-8 px-6 bg-white flex-1">
       <Header />
+      <Slider sliderList={sliderList}/>
     </View>
   )
 }
